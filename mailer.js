8c0db3818fc762ca60c913034c7010a75086609c
Mitch1000 <mitchell@fluidmedia.ca>
2018-04-01 19:34:08 -0400

    update for handling angular http

--- mailer.js
+++ mailer.js
@@ -1,46 +1,21 @@
-var http = require('http');
-var fs = require('fs'); var mkdirp = require('mkdirp'); 
-server = http.createServer( function(req, res) {
+const express = require('express')
+const bodyParser = require('body-parser');
+const app = express()
 
-    console.dir(req.param);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}) );
 
-    if (req.method == 'POST') {
-
-        console.log("POST");
-        var body = '';
-        req.on('data', function (data) {
-          body += data;
-          console.log("Partial body: " + body);
-
-          var info = JSON.parse(body);
-          fs.existsSync('./emails/' + info.email) || fs.mkdirSync('./emails/' + info.email);
-
-          fs.writeFileSync('./emails/' + info.email + '/' + new Date() + '.txt', 
-                          info.name + '\n \n' + info.email + '\n \n' + info.subject + '\n \n' + info.content, 
-                          'utf-8'); 
-          console.log('name: ' + info.name);
-        });
-
-        req.on('end', function () {
-          console.log("Body: " + body);
-        });
-
-        // Website you wish to allow to connect
-        res.setHeader('Access-Control-Allow-Origin', 'http://mitchell-drohan.com');
-    
-        // Request methods you wish to allow
-        res.setHeader('Access-Control-Allow-Methods', 'POST');
-    
-        // Request headers you wish to allow
-        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-        res.writeHead(200, {'Content-Type': 'html/text'});
-        res.end('post received');
-    }
-
+app.all("/*", function(req, res, next){
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  next();
 });
 
-port = 3000;
-host = '0.0.0.0';
-server.listen(port, host);
-console.log('Listening at http://' + host + ':' + port);
+app.post('/send_email', function (req, res) {
+  res.send(req.body)
+})
+
+app.listen(8080, function () {
+  console.log('Listenting on 8080!')
+})
