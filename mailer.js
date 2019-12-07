bc1d5beaab4e855eba38fb497a66c7588278fb0b
Mitch1000 <mitchell@fluidmedia.ca>
2019-12-07 15:17:41 -0500

    updated header format

--- mailer.js
+++ mailer.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({extended: true}) );
 app.all("/*", function(req, res, next){
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.header('Access-Control-Allow-Headers', 'Content-Length, X-Requested-With');
   next();
 });
 
